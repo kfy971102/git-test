ef5d524f41c38e45e76d6ea22d88fe8721b59fa1
kongfanyuan <853654055@qq.com>
2021-08-25 21:38:57 +0800

    first commit

---
+++ src/main/java/com/itheima/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @Author KFY
+ * @Data 2021/8/25 21:34
+ * @Description：
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
